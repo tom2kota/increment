0a881e80f943e1e29b77026fc3b0c64b45cfa76f
tom2kota <tom2kota@gmail.com>
2020-04-12 21:51:50 +0000

    Updates

---
+++ precache-manifest.091104ed1bfb06f5a2dcd9e1b55d5655.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25fabf744fae86ea843150e0b65f9411",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edf3bdd666eeb3cd81c2",
+    "url": "/static/js/2.34f61113.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.34f61113.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2e7505ccfef266b312",
+    "url": "/static/js/main.dadb0009.chunk.js"
+  },
+  {
+    "revision": "ff35e684d19a91852759",
+    "url": "/static/js/runtime-main.28d5c064.js"
+  },
+  {
+    "revision": "bdce90e8562656d2c56ea16fa3bb8cbe",
+    "url": "/static/media/WrappedCounter.bdce90e8.bin"
+  }
+]);
