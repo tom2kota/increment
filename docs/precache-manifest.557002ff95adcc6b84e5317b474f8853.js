0b862bd80b0aa39faea49ae36908386097ef06fe
Tom Kota <63372913+tom2kota@users.noreply.github.com>
2020-04-12 21:16:03 +0200

    Add files via upload

---
+++ docs/precache-manifest.557002ff95adcc6b84e5317b474f8853.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44545c46670dba358e261bd41a274a20",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7f96bf7a42ab51c26783",
+    "url": "./static/js/2.1909d4e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.1909d4e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "243e8713c52db7319686",
+    "url": "./static/js/main.ab55e377.chunk.js"
+  },
+  {
+    "revision": "a533d6febebc082edf16",
+    "url": "./static/js/runtime-main.64ec4c5f.js"
+  }
+]);
